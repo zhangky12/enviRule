4304bd84f7136f1ba57f4a23e6db77b4aef369bf
Kunyang Zhang <kunyang@host0555.wifi.ec-lyon.fr>
2022-10-14 17:55:44 +0200

    Add client for test

--- src/main/java/eawag/envirule/enviRule_demo.java
+++ src/main/java/eawag/envirule/enviRule_demo.java
@@ -9,10 +9,10 @@ public class enviRule_demo {
     public static void main(String[] args) throws Exception {
         boolean generalizeIgnoreHydrogen = true;
         boolean includeFunctionalGroups = true;
-        String file = "/Users/kunyang/Documents/Eawag/envipath/envipath-python/enviPath_python/BBD_reactions/Reactions4Rules/99-15.txt";
+//        String file = "/Users/kunyang/Documents/Eawag/envipath/envipath-python/enviPath_python/BBD_reactions/Reactions4Rules/99-15.txt";
 //        String file = "/Users/kunyang/Documents/Eawag/envipath/envipath-python/enviPath_python/BBD_autoRule_exp/run_1/reactions4rules/11-2.txt";
 //        String file = "/Users/kunyang/Documents/Eawag/envipath/envipath-python/enviPath_python/BBD_SOIL_autoRule_formal/bbd_soil_reactions4rules/699-6.txt";
-//        String file = "/Users/kunyang/Downloads/N-ring-break.txt";
+        String file = "/Users/kunyang/Downloads/N-ring-break.txt";
         int radius = 2;
 
         rule_generator generator = new rule_generator(generalizeIgnoreHydrogen, includeFunctionalGroups, file, radius);
